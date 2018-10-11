02bf7fece8e27b4dc02e6602e46f8629ba7b549d
rwj <heisenberg52599@outlook.com>
2018-10-11 18:53:01 +0800

    test

---
+++ yizu.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class yizu{
+    public static void main(String args[]){
+        Scanner reader = new Scanner(System.in);
+        double sum=0;
+        int m=0;
+        while(reader.hasNextDouble()){
+            double x=reader.nextDouble();
+            m=m+1;
+            sum=sum+x;
+        }
+        System.out.printf("%d个数的和为%f\n",m,sum);
+        System.out.printf("%d个数的平均值为%f\n",m,sum/m);
+        reader.close();
+    }
+}
